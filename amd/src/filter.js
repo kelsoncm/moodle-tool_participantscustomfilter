e729b731e2fecb9420a48db295d2356db3ccef51
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-03-04 14:28:19 -0300

    Update Moodle URLs to use HTTPS in all relevant files

--- amd/src/filter.js
+++ amd/src/filter.js
@@ -1,4 +1,4 @@
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -11,7 +11,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * AMD module for tool_participantscustomfilter.
@@ -22,7 +22,7 @@
  *
  * @module     tool_participantscustomfilter/filter
  * @copyright  2024 IFRN
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /**
